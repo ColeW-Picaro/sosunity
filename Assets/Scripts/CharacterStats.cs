0db2625f4fac2387190f61db050c58908913f262
HunterJohnson <30330298+BlizzardsDen@users.noreply.github.com>
2021-01-20 02:05:38 -0500

    CharacterWorkingHealthBar
    simple placeholder for character health has a gradient so when he gets to 60% hp it changes from green to yellow and at 20% it changes to red

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+
+    public int maxHealth = 100; //sets a characters max hp
+    public int currentHealth; //changes characters health
+
+    public HealthBar healthBar; //connects healthbar script to character
+
+    void Start()
+    {
+        currentHealth = maxHealth; //sets currenthealth to maxhealth
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) //pressing space makes character take 20 damage
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage; //currenthealth minus how much damage he takes
+
+        healthBar.SetHealth(currentHealth); //changes the slider to currenthealth
+    }
+}
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthBar;
+    public Gradient healthBarGradient;
+    public Image fill;
+
+    public void SetMaxHealth(int health)
+    {
+        healthBar.maxValue = health;
+        healthBar.value = health;
+
+        fill.color = healthBarGradient.Evaluate(1f);
+    }
+
+    public void SetHealth(int health)
+    {
+        healthBar.value = health;
+
+        fill.color = healthBarGradient.Evaluate(healthBar.normalizedValue);
+    }
+
+}
