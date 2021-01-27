d88364cb575fe615ed3d664cb41abf728d2eadbc
booperxd <coopervohs@gmail.com>
2021-01-27 00:35:45 -0500

    Switching scenes through portals

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SwitchScene : MonoBehaviour
+{
+    public Text portalText;
+    public string destination;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            portalText.text = "Enter: " + destination;
+            if (Input.GetAxisRaw("Interact") == 1) {
+                SceneManager.LoadScene(destination);
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            if (Input.GetAxisRaw("Interact") == 1) {
+                SceneManager.LoadScene(destination);
+            }
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            portalText.text = "";
+        }
+    }
+}
